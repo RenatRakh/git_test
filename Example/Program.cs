b933d468916b0fbf2523724dcff087f7fd96a5f2
Renat <rrenat14@gmail.com>
2022-09-15 11:05:43 +0500

    h3 was added!

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace Example
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GIT");
+            Console.WriteLine("Hello GIT 2");
+            Console.WriteLine("Hello GitHub");
         }
     }
 }
